69c880c312e78030b78db58e6c05cbc25223806f
Buzzzzzzzzzzzzzzzzz <basile.dhauthuille@etu.estia.fr>
2025-05-28 15:02:51 +0200

    fix2

--- src/features.h
+++ src/features.h
@@ -1,10 +1,6 @@
 #ifndef FEATURES_H
 #define FEATURES_H
 
-void dimension (char *source_path);
-
-void first_pixel (char *source_path);
-
 void helloWorld();
 void dimension (const char *source_path);
 void first_pixel (const char *source_path);
--- src/main.c
+++ src/main.c
@@ -41,5 +41,14 @@ int main(int argc, char **argv) {
     /* first_pixel() function is defined in feature.h and implemented in feature.c */
     first_pixel( configuration.filenames[0] );
 }
+
+  if ( strncmp( configuration.command, "tenth_pixel", 12 ) == 0 ) {
+    /* second_line() function is defined in feature.h and implemented in feature.c */
+    tenth_pixel( configuration.filenames[0] );
+}
+  if ( strncmp( configuration.command, "second_line", 13 ) == 0 ) {
+    /* second_line() function is defined in feature.h and implemented in feature.c */
+    second_line( configuration.filenames[0] );
   return 0;
+}
 }
