3d967cdcef6763a579cc4b734b2afaa2cc4c8dcc
ArthurMUREAU <arthur.mureau@etu.estia.fr>
2025-06-19 21:43:56 +0200

    fix : problème d'entête

--- src/main.c
+++ src/main.c
@@ -120,13 +120,13 @@ if ( strncmp( configuration.command, "mirror_total", 12 ) == 0 ) {
   mirror_total( configuration.filenames[0] );
 }
 if ( strncmp( configuration.command, "scale_nearest", 15 ) == 0 ) {
-scale_nearest( configuration.filenames[0], configuration.arguments[0] );
+scale_nearest( configuration.filenames[0], atof(configuration.arguments[0]) );
 }
 if ( strncmp( configuration.command, "scale_bilinear", 15 ) == 0 ) {
-scale_bilinear( configuration.filenames[0], configuration.arguments[0] );
+scale_bilinear( configuration.filenames[0], atof(configuration.arguments[0]) );
 }
 if ( strncmp( configuration.command, "scale_crop", 15 ) == 0 ) {
-scale_crop( configuration.filenames[0], configuration.arguments[1],configuration.arguments[2], configuration.arguments[3], configuration.arguments[4]);
+scale_crop( configuration.filenames[0], atoi(configuration.arguments[0]),atoi(configuration.arguments[1]), atoi(configuration.arguments[2]),atoi(configuration.arguments[3]));
 }
 
 return 0;
